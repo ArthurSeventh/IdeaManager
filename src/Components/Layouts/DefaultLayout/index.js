e485bc379101d109e754775e7b2ce00a72293805
ArthurSeventh <hao.vv2001@gmail.com>
2023-03-23 22:16:13 +0700

    Initial Push

---
+++ src/Components/Layouts/DefaultLayout/index.js
@@ -0,0 +1,16 @@
+import Sidebar from './Siderbar'
+import Header from './Components/Header'
+
+function DefaultLayout({children}) {
+    return (
+        <div className='wrapper'>
+            <Header />
+            <div className='Container'>
+                <Sidebar />
+                <div className='Content'>{children}</div>
+            </div>
+        </div>
+    )
+}
+
+export default DefaultLayout;
